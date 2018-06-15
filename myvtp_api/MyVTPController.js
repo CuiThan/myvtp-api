beb17b5610fc1e175ec66ef23b186fa3e16681dc
Cu Phan <cuph.it1@gmail.com>
2018-06-15 14:09:03 +0700

    1

--- myvtp_api/MyVTPController.js
+++ myvtp_api/MyVTPController.js
@@ -81,4 +81,34 @@ router.post('/push-order-status',VerifyToken, function(req, resp) {
     });
 });
 
+router.get('/get-order-status',VerifyToken, function(req, resp) {
+    if (req.body == undefined || Object.keys(req.body).length == 0)
+    {
+        resp.status(200).send('orderInfo is required!!!');
+        return;
+    }
+
+    // err === null -> valid
+    Joi.validate(JSON.stringify(req.body).toLowerCase(), ValidateOrderStatus, function (err, value) {
+        if (err === null) {
+            var topicName = Setting.TOPIC_NAME_ORDER;
+            var kafkaKey = Setting.KAFKA_KEY;
+            var kafkaObject = new Object();
+            kafkaObject.type = APIType.PUSH_ORDER_STATUS;
+            kafkaObject.data = req.body;
+            var kafkaValue = JSON.stringify(kafkaObject).toLowerCase();
+
+            var rs = new KafkaService();
+            rs.sendMessage([{topic: topicName,
+                messages: kafkaValue,
+                key: kafkaKey}], resp);
+        }
+        else {
+            //error
+            err.status = "ValidateError";
+            resp.status(200).send(err);
+        };
+    });
+});
+
 module.exports = router;
--- validation/order-status.js
+++ validation/order-status.js
@@ -2,8 +2,8 @@ var Joi = require('joi');
 
 var validateOrderStatus = {
     order_number : Joi.string().required(),
-    order_status : Joi.number().required(),
-    tracking_time : Joi.number().required(),
+    order_status : Joi.number().optional(),
+    tracking_time : Joi.number().optional(),
     post_id : Joi.number().optional().allow(null),
     tracking_note : Joi.string().optional().allow(null),
     cus_id : Joi.number().optional().allow(null),
